d76c91f67a2b228f3e8d29acfca9bd5c6d593353
Alberto-Cugat <67785595+Alberto-Cugat@users.noreply.github.com>
2021-01-23 14:06:23 +0100

    Segundo update de la nueva aventura
    Despues de MATAR A SANGRE FRIA a todos los pokamions que se metian en mi camino he logrado llegar hasta el primer gimnasio.
    El pavo estaba mazado pero ningun problema, tengo mis cheats asi que he capturado sus pokamions y se ha echado a llorar. Que patetico, casi hilarante.
    Me sentia como un ugly bastard en un hentai de NTR xDD el rastro de desesperacion en sus rostron me ponian cachondo.

--- Aventura 3/test.sh y pilas para pruebas/av3.c
+++ Aventura 3/test.sh y pilas para pruebas/av3.c
@@ -8,25 +8,37 @@ Rubén Palmer Pérez
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_THREADS 10
+int pila;
+
 char *checkAvailability(filename){
     //se coge por consola el nombre del archivo i se guarda en filename (PENDIENTE)
-    int pila = open(filename, O_RDONLY , S_IRUSR); //supongo que si el archivo no 
+    pila = open(filename, O_RDONLY , S_IRUSR); //supongo que si el archivo no 
     
     if(pila < 0){
         close(pila);
         return NULL;
     }
-    return filename 
+    return filename;
 }
 
 int main(){
     struct my_stack *pila;
+    FILE *filepointer;
     char filename;
     int len = 0;
+    
+    if(fgets(filename, 200, filepointer) == NULL){  
+        fprintf(stderr,"Failed to read the filename");                                                 // Non EOF command
+        exit(EXIT_FAILURE);
+    }
     if((filename = checkAvailability(filename)) == NULL){ //la pila no existe
         pila = my_stack_init(malloc(sizeof(int))); //cogemos el puntero del objeto pila
-        int fichero = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); //aqui queremos crear el archivo (PENDIENTE)
+        filepointer = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); //aqui queremos crear el archivo (PENDIENTE)
         len = 10;
+        for(int i = 0; i < len; i++){ //llenamos las posiciones necesarias de la pila con 0s (si la pila existe no hay que machcarla sino usarla)
+        my_stack_push(pila, 0);
+    }
     }else{ //la pila si existe
         pila = my_stack_read(filename); //aqui leemos la pila del archivo y guardamos el puntero del objeto
         len = my_stack_len(pila); //cogemos el tamaño de la pila
@@ -35,7 +47,19 @@ int main(){
             len = 0;
         }      
     }
-    for(int i = 0; i < len; i++){ //llenamos las posiciones necesarias de la pila con 0s
-        my_stack_push(pila, 0);
+
+    pthread_t arrayThreads [10];
+
+    for(int i = 0; i < NUM_THREADS; i++){
+       pthread_create(arrayThreads[i],NULL,thread,NULL);
     }
+
+    for (int i = 0; i < NUM_THREADS; i++){
+        pthread_join(arrayThreads[i],NULL);
+    }
+    
+}
+
+int thread(){
+    
 }
